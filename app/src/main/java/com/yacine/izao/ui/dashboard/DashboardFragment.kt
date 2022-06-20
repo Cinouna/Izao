587f3fedd28eb39de4582f00c741c51e67fdfcc2
cinouna <dppaapp2@gmail.com>
2022-06-20 13:50:17 +0200

    Boutton vacance (version Emir)

--- app/src/main/java/com/yacine/izao/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/yacine/izao/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,20 @@
 package com.yacine.izao.ui.dashboard
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.yacine.izao.R
+import com.yacine.izao.ui.dashboard.DashboardViewModel
 import com.yacine.izao.databinding.FragmentDashboardBinding
+
+
+
+
+
 
 class DashboardFragment : Fragment() {
 
@@ -22,18 +29,44 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        binding.toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
 
-        val textView: TextView = binding.textDashboard
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnAndroid -> showToast("Robot that looks like human.")
+                    R.id.btnFlutter -> showToast("It's a Butterfly thing.")
+
+                }
+            } else {
+                if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
+                    showToast("Nothing Selected")
+                }
+            }
+        }
+
+
+
+/*
+        val textView: TextView = binding.textdashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
+
+
+
     }
+
+    private fun showToast(str: String) {
+        Toast.makeText(activity?.baseContext, str, Toast.LENGTH_SHORT).show()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/yacine/izao/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/yacine/izao/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,20 @@
 package com.yacine.izao.ui.dashboard
-
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yacine.izao.R
+
 
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
+
     }
     val text: LiveData<String> = _text
+
+
 }
